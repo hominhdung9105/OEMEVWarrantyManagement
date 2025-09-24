29cd811a5a4628ee7014c76cad5fc746bd0f946a
PhucLamHong <phuclamhong05@gmail.com>
2025-09-24 22:16:25 +0700

    merge architecture and format  response

--- OEMEVWarrantyManagement.Application/Dtos/TokenResponseDto.cs
+++ OEMEVWarrantyManagement.Application/Dtos/TokenResponseDto.cs
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:OEMEVWarrantyManagement.Application/Dtos/TokenResponseDto.cs
 ﻿namespace OEMEVWarrantyManagement.Application.Dtos
-========
-﻿namespace OEMEVWarrantyManagement.API.Models.Response
->>>>>>>> Format-Response:OEMEVWarrantyManagement.API/Models/Response/TokenResponseDto.cs
 {
     public class TokenResponseDto
     {
